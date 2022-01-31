c33e3a784e7db3d1e0242fb37199db7f842b9d10
Justyn Durnford <jpdurnford@alaska.edu>
2022-01-30 15:09:59 -0900

    Made my first Python project. Just a simple I/O program.

---
+++ FirstPythonApplication/FirstPythonApplication.py
@@ -0,0 +1,16 @@
+# FirstPythonApplication
+# FirstPythonApplication.py
+# Created on 2022-01-30 by Justyn Durnford
+# Last modified on 2022-01-30 by Justyn Durnford
+# Simple first project using I/O.
+
+def function1():
+	print("Hello there!");
+	input_str = input("Enter a message: ")
+	print("You entered: " + input_str)
+
+def main():
+	function1()
+
+if __name__ == "__main__":
+   main()
